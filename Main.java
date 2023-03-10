e8fdeca470c6dd8bc40f7720a6fe29a29bd2ab51
Irina <airin-lia@mail.ru>
2023-03-10 20:16:15 +0300

    0

---
+++ Main.java
@@ -0,0 +1,16 @@
+package OOP_HW2;
+
+import java.util.LinkedList;
+
+public class Main {
+    private LinkedList<Animal> zoo = new LinkedList<>();
+
+    public LinkedList<Animal> getZoo() {
+        return zoo;
+    }
+
+    public void setZoo(LinkedList<Animal> zoo) {
+        this.zoo = zoo;
+    }
+
+}
